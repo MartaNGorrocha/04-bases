761ba72fa1df808ca077061565721958e5e2665e
Naveira Gorrochategui <mnaveira@minsait.com>
2024-10-10 17:29:59 +0200

    Fin de sección 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeroComponent } from './heroes/hero/hero.component';
+import { ListComponent } from "./heroes/list/list.component";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = '04-bases';
+  title = '01-bases-new';
 }
